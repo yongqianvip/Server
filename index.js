e6158ef06360dccffbe80e2661863183a34156b8
yinyongqian <yinyongqian@renrenche.com>
2020-05-21 09:47:43 +0800

    remove node_module and gitignore

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var app = express();
 var clientList = require('./src/json/clientList.json');
 
 app.use('/public', express.static('public'));
- 
+
 app.get('/index.htm', function (req, res) {
   console.log(__dirname);
    res.sendFile( __dirname + "/src/" + "index.htm" );
 })
 /**
- * 
+ *
  */
 app.get('/login', function (req, res) {
   console.log(req.query);
@@ -36,7 +36,7 @@ app.get('/login', function (req, res) {
   }, 1000);
 })
 /**
- * get user  info 
+ * get user  info
  */
 app.get('/user', function(req, res){
   setTimeout(() => {
@@ -47,7 +47,7 @@ app.get('/user', function(req, res){
 // 客户列表
 app.get('/clientList', function(req, res){
   console.log(req.query);
-  var params = req.query; 
+  var params = req.query;
   var page = parseInt(params ? params['page'] : 0);
   var allClient = clientList['list'];
   var targetClient = [];
@@ -64,7 +64,7 @@ app.get('/clientList', function(req, res){
     res.send(JSON.stringify(response));
   }, 4000);
 })
- 
+
 app.get('/checkVersion', function(req, res){
   setTimeout(() => {
     res.sendFile(__dirname + "/src/json/version.json")
@@ -80,7 +80,7 @@ app.get('/getHomeData', function(req, res){
   }
   setTimeout(() => {
     if (params.all) {
-      res.sendFile(__dirname + "/src/json/runwaygo/home.json")  
+      res.sendFile(__dirname + "/src/json/runwaygo/home.json")
     }else{
       res.sendFile(__dirname + "/src/json/runwaygo/home" + params.page + ".json")
     }
@@ -89,7 +89,7 @@ app.get('/getHomeData', function(req, res){
 
 app.get('/searchResultList', function(req, res){
   setTimeout(() => {
-    res.sendFile(__dirname + "/src/json/runwaygo/search/searchResult.json") 
+    res.sendFile(__dirname + "/src/json/runwaygo/search/searchResult.json")
   }, 1000);
 })
 // "header": {
@@ -102,7 +102,7 @@ app.get('/productDetail', function(req, res){
   setTimeout(() => {
     res.sendFile(__dirname + "/src/json/runwaygo/product/detail.json")
   }, 500)
-  
+
 })
 
 
@@ -110,14 +110,14 @@ app.get('/getAddressList', function(req, res){
   setTimeout(() => {
     res.sendFile(__dirname + "/src/json/runwaygo/mine/address.json")
   }, 500)
-  
+
 })
 
 app.get('/getPayInfo', function(req, res){
   setTimeout(() => {
     res.sendFile(__dirname + "/src/json/runwaygo/pay/payInfo.json")
   }, 1000)
-  
+
 })
 
 
@@ -156,12 +156,24 @@ app.get('/getUnionPayData', function(req, res){
 })
 
 
+app.get('/getHotSearchList', function(req, res) {
+  setTimeout(() => {
+    res.sendFile(__dirname + "/src/json/rayCloud/hotSearchList.json")
+  }, 500);
+})
+
+app.get('/keywordAssociate', function(req, res) {
+  setTimeout(() => {
+    res.sendFile(__dirname + "/src/json/rayCloud/keywordAssociate.json")
+  }, 500);
+})
+
 var server = app.listen(8080, function () {
- 
+
   var host = server.address().address
   var port = server.address().port
   console.log(server.address());
-  
+
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
   // console.log(clientList);
 })
